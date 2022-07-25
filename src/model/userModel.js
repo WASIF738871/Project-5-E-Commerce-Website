bfe1c9244c95178d525bf5198be68a0ab74f8746
bidyut10 <bidyut34268@gmail.com>
2022-07-25 10:52:56 +0530

    index,route,user

---
+++ src/model/userModel.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+
+    fname: {
+        type: String,
+        required: true
+    },
+    lname: {
+        type: String,
+        required: true
+    },
+
+    phone: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        require: true,
+        type: String,
+        unique: true
+    },
+    password: {
+        require: true,
+        type: String,
+        minlength: 8,
+
+
+    },
+    profileImage: {
+        type: String,
+        required: true
+    },
+    address: {
+        shipping: {
+            street: { type: String },
+            city: { type: String },
+            pincode: { type: String }
+        }
+        ,
+        billing: {
+            street: { type: String },
+            city: { type: String },
+            pincode: { type: String }
+        }
+    }
+
+}, { timestamps: true });
+
+
+module.exports = mongoose.model('users', userSchema)
