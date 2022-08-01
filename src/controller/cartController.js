f077ea3bf0ee5ecdb3810c7397f3d2226fc45c8b
Mohd-Farhan-9990 <mohdfarhan9639@gmail.com>
2022-08-01 19:16:41 +0530

    Cart Create Done

--- src/controller/cartController.js
+++ src/controller/cartController.js
@@ -7,8 +7,10 @@ const addCart = async function(req,res){
 
         let userId = req.params.userId
         let data = req.body
-        
-        let{productId,quantity} = data
+            let {items} = data
+            let productId = items[0].productId
+
+//-----------------------------Checking User
             if(!isValidObjectId(userId)){
                 return res.status(400).send({status:false,msg:"Invalid UserId"})
             }
@@ -20,41 +22,59 @@ const addCart = async function(req,res){
                     //     return res.status(403).send({status:false,msg:"Unauthorizes Acces"})
                     // }
             data['userId'] = userId
+
+
+//---------------------------Product Id Validation            
             if(!isValidObjectId(productId)){
                 return res.status(400).send({status:false,msg:"Invalid Product Id"})
             }
                 let prodExist = await productModel.findById({_id:productId,isDeleted:false})
+                console.log(prodExist)
                     if(!prodExist){
                         return res.status(404).send({status:false,msg:"Either Product is Deleted or Doesn't Exist"})
                     }
-                
-                let totalPrice = prodExist.price
-                data['totalPrice'] = totalPrice
-                let totalItems = 1
-                data['totalItems'] = totalItems
-        
-        let isCart = await cartModel.findOne({useId:userId})
-        totalPrice = isCart.totalPrice+totalPrice
-        totalItems = isCart.totalItems+totalItems
+                    let totalPrice = prodExist.price
+                    let totalItems = 1
 
+             
+        
+        let isCart = await cartModel.findOne({userId:userId})
         if(!isCart){
+            data['totalPrice'] = totalPrice
+            data['totalItems'] = totalItems
             let createCart = await cartModel.create(data)
             return res.status(201).send({status:true,msg:"Cart Created Succesfullt",data:createCart})
         }
         else{
-            let prod= {}
+            let x=0
             let newObj = {}
-            newObj['totalPrice'] = totalPrice
-            newObj['totalItems'] = totalItems
-            console.log(isCart.items[0].productId)
-            for(let i=0; i<isCart.items.lenght; i++){
-            if(isCart.items.productId!=productId){
-                prod['productId']= productId
+            // console.log(isCart)
+            for(let i=0; i<isCart.items.length; i++){
+            if(isCart.items[i].productId==productId){
+                 isCart.items[i].quantity += parseInt(items[0].quantity)
+                x=1
+                break;
+                    
             }
-            quantity = isCart.items.quantity+quantity
             }
-            prod['quantity'] = quantity
-            let addtoCart = await cartModel.findOneAndUpdate({userId:userId},{$set:newObj,$push:{items:prod}},{new:true})
+            if(x==0){
+                let obj = {
+                    productId:items[0].productId,
+                    quantity:items[0].quantity
+                }
+                console.log(obj)
+                isCart.items.push(obj)
+                console.log("Hlw1")
+                console.log(isCart)
+            }
+        
+
+             isCart.totalPrice += totalPrice
+            // newObj['totalPrice'] = totalPrice
+            isCart.totalItems = isCart.items.length
+            // newObj['totalItems'] = totalItems
+            // console.log(isCart)
+            let addtoCart = await cartModel.findOneAndUpdate({userId:userId},{$set:isCart},{new:true})
             return res.status(200).send({status:true,data:addtoCart})
         }
 }
